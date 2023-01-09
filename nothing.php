217d5e3c02f2a3d69558b97c4b3bd897a9827573
Kristaps <esmitskristaps@inbox.lv>
2023-01-10 00:35:39 +0200

    app download

--- nothing.php
+++ nothing.php
@@ -3,14 +3,13 @@ include 'backend/Auth.php';
 require('backend/db_con.php');
 ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Preces</title>
+        <title>News</title>
         <link rel="icon" href="resources/favicons/fav.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -20,7 +19,11 @@ require('backend/db_con.php');
 <body>
     <div>
         <img class="img" src="resources/img/s.png" alt="zzz">
-        <h1>Nothing to see here!<h1>
+        <h1>Nothing to see here!</h1>
+        <a href="https://drive.google.com/uc?export=download&id=1tccWMVvatjucFSWpTJNhMvHCIap6cvYP" target="_blank" rel="noopener noreferrer">
+            <button>Download desktop app!</button>
+        </a>
+
     </div>
 </body>
 </html>
