a09a2e9135b6390b49dc2b22d4f27df188455d87
Kris <kristiansberzins@outlook.lv>
2023-01-14 16:34:50 +0200

    getting inventarizacija ready for editing

--- inventarizacija.php
+++ inventarizacija.php
@@ -18,50 +18,6 @@ require('backend/db_con.php');
     </head>
 <body>
     <div class="Fields">
-        <button id="add-btn">Pievienot Preci</button>
-        <form action="" method="post">
-            <div id="add-pop">
-                <input name="preces_nosaukums" type="text" class="input" placeholder="Nosaukums" required>
-                <label class="ievesana">Ievešanas datums:</label>
-                <input name="datums" type="date" class="input" placeholder="Ievešanas datums" value="Ievešanas datums" required>
-                <label class="termins">Termiņš:</label>
-                <input name="termins" type="date" class="input" placeholder="Termiņš">
-                <input name="cena_bez_PVN" type="text" class="input" placeholder="Cena bez PVN" required>
-                <select class="sinput" name="pvn_izvele">
-                    <option value="21">21%</option>
-                    <option value="12">12%</option>
-                    <option value="5">5%</option>
-                </select>
-                <input name="skaits" type="text" class="input" placeholder="Skaits" required>
-                <input name="daudzums" type="text" class="input" placeholder="Pārdotais daudzums" required>
-                <select class="sinput" name="preces_kategorija">
-                    <?php
-                        $query = "SELECT * FROM kategorijas";
-                        $result = mysqli_query($con,$query);
-                        while($row = mysqli_fetch_array($result)) {
-                        ?>
-                        <option name="preces_NR" value=<?php echo $row["Kategorijas_ID"]; ?>><?php echo $row["Nosaukums"]; ?></option>
-                        <?php
-                        }
-                    ?>
-                </select>
-                <select class="sinput" name="plaukta_id">
-                    <?php
-                        $query = "SELECT * FROM noliktava
-                                  LEFT JOIN kategorijas
-                                         ON kategorijas.Kategorijas_ID = noliktava.Kategorijas_ID";
-                        $result = mysqli_query($con,$query);
-                        while($row = mysqli_fetch_array($result)) {
-                        ?>
-                        <option name="plaukta_nr" value="<?php echo $row["Plaukta_ID"]; ?>"><?php echo "Plaukts ar ID " . $row["Plaukta_ID"] . ", sektors " . $row["Sektors"] . ", stāvs " . $row["Stavs"] . ", kategorija " . $row["Nosaukums"]; ?></option>
-                        <?php
-                        }
-                    ?>
-                </select>
-                <input class="btn" name=submit type="submit" value="Pievienot">
-                <button id="close-btn">Atcelt</button>
-            </div>
-        </form>
     </div>
     <div class="list">
         <div class="tabulaBox">
