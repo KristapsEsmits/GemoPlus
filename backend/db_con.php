0dae51fc1d404f4b0e78972d1e63301850b60fcc
Kris <kristiansberzins@outlook.lv>
2023-02-02 09:08:50 +0200

    comment

--- backend/db_con.php
+++ backend/db_con.php
@@ -1,5 +1,6 @@
 <?php
-$con = mysqli_connect("localhost:3308","root","","gemoplus"); #Maini šeit ja vajag, tikai db nosaukumu ,ja izmanto xampp parejo atstaj tadu pašu!
+// if shows port error try to change localhost to localhost:3308
+$con = mysqli_connect("localhost","root","","gemoplus"); #Maini šeit ja vajag, tikai db nosaukumu ,ja izmanto xampp parejo atstaj tadu pašu!
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
