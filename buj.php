46af87ce1c0c5d7e7589a7ba39298ccbe57ba6d5
Kristaps <esmitskristaps@inbox.lv>
2023-02-10 11:18:16 +0200

    buj page done?

--- buj.php
+++ buj.php
@@ -4,9 +4,76 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Biežāk uzdotie jautājumi</title>
+    <link rel="icon" href="resources/favicons/fav.png" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;1,700&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="resources/css/buj.css"/>
 </head>
 <body>
+    <h1 class="heading">Biežāk uzdotie jautājumi</h1>
+    <section class="faq-container">
+        <div>
+            <h1 class="faq-page">Jautājums Nr1?</h1>
+            <div class="faq-body">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit saepe sequi, illum facere
+                    necessitatibus cum aliquam id illo omnis maxime, totam soluta voluptate amet ut sit ipsum
+                    aperiam.
+                    Perspiciatis, porro!</p>
+            </div>
+        </div>
+        <hr class="hr-line">
+        <div>
+            <h1 class="faq-page">Garāks jautājums NR2?</h1>
+            <div class="faq-body">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit saepe sequi, illum facere
+                    necessitatibus cum aliquam id illo omnis maxime, totam soluta voluptate amet ut sit ipsum
+                    aperiam.
+                    Perspiciatis, porro!</p>
+            </div>
+        </div>
+        <hr class="hr-line">
+        <div>
+            <h1 class="faq-page">Savādāks jautājums NR3?</h1>
+            <div class="faq-body">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit saepe sequi, illum facere
+                    necessitatibus cum aliquam id illo omnis maxime, totam soluta voluptate amet ut sit ipsum
+                    aperiam.
+                    Perspiciatis, porro!</p>
+            </div>
+        </div>
+        <hr class="hr-line">
+        <div>
+            <h1 class="faq-page">Pavisam cits jautājums NR4?</h1>
+            <div class="faq-body">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit saepe sequi, illum facere
+                    necessitatibus cum aliquam id illo omnis maxime, totam soluta voluptate amet ut sit ipsum
+                    aperiam.
+                    Perspiciatis, porro!</p>
+            </div>
+        </div>
+        <hr class="hr-line">
+        <h1 class="faq-page">Ļoti labs jautājums NR5?</h1>
+        <div>
+            <div class="faq-body">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit saepe sequi, illum facere
+                    necessitatibus cum aliquam id illo omnis maxime, totam soluta voluptate amet ut sit ipsum
+                    aperiam.
+                    Perspiciatis, porro!</p>
+            </div>
+        </div>
+        <hr class="hr-line">
+        <div>
+            <h1 class="faq-page">Interesants jautājums NR6?</h1>
+            <div class="faq-body">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit saepe sequi, illum facere
+                    necessitatibus cum aliquam id illo omnis maxime, totam soluta voluptate amet ut sit ipsum
+                    aperiam.
+                    Perspiciatis, porro!</p>
+            </div>
+        </div>
+    </section>
     <script src="resources/js/index.js"></script>
 </body>
 </html>
