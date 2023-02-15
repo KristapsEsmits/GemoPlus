bef0a654d491d8e0ff3d58c9c74bfbe98334d332
Kris <kristiansberzins@outlook.lv>
2023-02-15 13:57:16 +0200

    izlaboju buj (kristaps)

--- buj.php
+++ buj.php
@@ -17,30 +17,30 @@
         <div>
             <h1 class="jautajums">Kur ir atrodama lietotāja rokasgrāmata?</h1>
             <div class="buj-atbilde">
-                <p>Lietottāja rokasgrāmatu ir iespējams atrast <a href ="Lietotaju_instrukcija.docx" download>šeit</a>!</p>
+                <p>Lietottāja rokasgrāmatu ir iespējams atrast <a href ="Lietotaju_instrukcija.pdf" download>šeit</a>!</p>
             </div>
         </div>
         <hr class="hr-line">
         <div>
             <h1 class="jautajums">Kā ir iespējams reģistrēties?</h1>
             <div class="buj-atbilde">
-                <p>Diemžēl pašam nav iespējams reģistrēties! Lai reģistrētu jaunus lietotājus jautā savam administrātoram, kas to arī paveiks :3</p>
+                <p>Lai reģistrētu jaunus lietotājus sazinies ar administratoru <a href="https://discord.gg/7RMSnJPu" target="blank">šeit!</a></p>
             </div>
         </div>
         <hr class="hr-line">
         <div>
             <h1 class="jautajums">Vai ir iespējams kārtot datus?</h1>
             <div class="buj-atbilde">
-                <p>Lai kārtotu datus dodies uz kādu no sadaļām, kur to vēlies darīt. Pēc tam noklikšķini uz attiecīgās tabulas galvenes , kuru vēlies kārtot.</p>
-                <p>Vienreiz noklikšķinot dati tiks kārtoti A-Z ņemot izvēlēto tabulas aili kā primāro kārtošanas objektu.</p>
-                <p>Otrreiz noklikšķinot dati tiks kārtoti Z-A, ņemot izvēlēto tabulas aili kā primāro kārtošanas objektu.</p>
+                <p>Datus ir iespējams kārtot noklikšķinot uz attiecīgās tabulas galvenes kollonas.</p>
+                <p>Vienreiz noklikšķinot dati tiks kārtoti A-Z.</p>
+                <p>Otrreiz noklikšķinot dati tiks kārtoti Z-A.</p>
             </div>
         </div>
         <hr class="hr-line">
         <div>
             <h1 class="jautajums">Vai ir iespējams atgriezt nejauši dzēstos ierakstus?</h1>
             <div class="buj-atbilde">
-                <p>Diemžēl nav iespējams atjaunot dzēstos ierakstus!</p>
+                <p>Nē, nav iespējams atjaunot dzēstos ierakstus!</p>
             </div>
         </div>
         <hr class="hr-line">
@@ -48,15 +48,14 @@
             <h1 class="jautajums">Es nespēju pievienot vai dzēst darbiniekus?</h1>
             <div class="buj-atbilde">
                 <p>Lai varētu rīkoties ar darbinieku datiem ir jābūt administrātora privilēgijām.</p>
-                <p>Ja tev ir jābūt šādai peejai un tev tās nav kontaktējies ar kādu no administrātoriem!</p>
+                <p>Ja tev ir jābūt šādai peejai un tev tās nav kontaktējies ar administrātoru <a href="https://discord.gg/7RMSnJPu" target="blank">šeit!</a></p>
             </div>
         </div>
         <hr class="hr-line">
         <div>
             <h1 class="jautajums">Nespēju atcerēties savu lietotājvārdu un/vai paroli?</h1>
             <div class="buj-atbilde">
-                <p>Ja nespējat atcerēties savu lietotājvārdu un/vai paroli, sazinaties ar uzņēmuma administratoru,
-                    kā arī viņš palīdzēs tādos gadījumos, ja jūsu lietotājvārds un/vai parole ir nodota trešajām personām, uzstādot jūsu lietotājvārdam jaunu paroli.</p>
+                <p>Ja nespējat atcerēties savu lietotājvārdu un/vai paroli, sazinaties ar administratoru <a href="https://discord.gg/7RMSnJPu" target="blank">šeit!</a></p>
             </div>
         </div>
     </section>
